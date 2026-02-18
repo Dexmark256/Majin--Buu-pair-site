657b531ef8d39c0f1f02ff4e3b6b414abf30e613
Dexmark Tech <dexmarkdexmark@gmail.com>
2026-02-18 13:57:47 +0300

    Refactor server setup and add HTML routes
    Refactored server setup, updated port, and added routes for serving HTML files.

--- index.js
+++ index.js
@@ -1,20 +1,29 @@
 const express = require('express');
-const path = require('path');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+__path = process.cwd()
+const bodyParser = require("body-parser");
+const PORT = process.env.PORT || 8000;
+let server = require('./qr'),
+    code = require('./pair');
+require('events').EventEmitter.defaultMaxListeners = 500;
+app.use('/server', server);
+app.use('/code', code);
+app.use('/pair',async (req, res, next) => {
+res.sendFile(__path + '/pair.html')
+})
+app.use('/qr',async (req, res, next) => {
+res.sendFile(__path + '/qr.html')
+})
+app.use('/',async (req, res, next) => {
+res.sendFile(__path + '/main.html')
+})
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.listen(PORT, () => {
+    console.log(`
+Don't Forgot To Give Star Majin-Buu 
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+ Server running on http://localhost:` + PORT)
+})
 
-// UI
-app.get('/pair-ui', (req, res) => {
-  res.sendFile(path.join(__dirname, 'pair.html'));
-});
-
-// API
-app.use('/pair', require('./pair'));
-
-app.get('/', (req, res) => res.redirect('/pair-ui'));
-
-app.listen(PORT, () => console.log('Server running on ' + PORT));
+module.exports = app
